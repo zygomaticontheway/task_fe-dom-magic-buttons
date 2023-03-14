5441c249a1b38edeb90e4cec030c79b76a71bd10
Alisher Khamidov <110631572+AlisherKhamidov@users.noreply.github.com>
2023-03-14 17:25:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// найдем первую кнопку по id
+const btn = document.getElementById("magic-btn");
+// скопируем ее - получилась вторая кнопка. Ее можно использовать.
+const clonedBtn = btn.cloneNode(true);
+// задали id
+clonedBtn.id = "magic-btn-2";
+
+btn.addEventListener('click', () => {
+  // при нажатии на первую кнопку
+  // работайте с clonedBtn
+  // ваш код начинается здесь
+  
+})
+
+// здесь можете создать EventListener для второй кнопки
