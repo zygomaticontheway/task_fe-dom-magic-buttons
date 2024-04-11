164c923c29a70eb7696779b357736964cd8cb339
zygomaticontheway <mrzygomatic@gmail.com>
2024-04-11 09:55:54 +0200

    разбор домашки преподом

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
+  clonedBtn.textContent = "я изменю тебя";
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
+  clonedBtn.classList.add("second-btn");
+  document.body.append(clonedBtn);
 })
 
 // здесь можете создать EventListener для второй кнопки
+let toggle = true;
+clonedBtn.addEventListener("click", ()=>{
+  if (toggle){
+    btn.style.backgroundColor = "#9c4a1a";
+    btn.style.color = "black";
+  }
+  else {
+    btn.style.backgroundColor = "#f7c815";
+    btn.style.color = "white";
+  }
+  toggle = !toggle
+})
